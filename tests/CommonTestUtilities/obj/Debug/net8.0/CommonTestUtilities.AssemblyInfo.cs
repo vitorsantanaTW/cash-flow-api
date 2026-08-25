6389320f22bbbd7577e723d02360bd1112c57003
vitorSantanaTw <vitor.santana@thoughtworks.com>
2026-08-25 14:32:28 -0700

    saving changes

--- tests/CommonTestUtilities/obj/Debug/net8.0/CommonTestUtilities.AssemblyInfo.cs
+++ tests/CommonTestUtilities/obj/Debug/net8.0/CommonTestUtilities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommonTestUtilities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f26e15ce864338aa7890a03bb4d750fca7b2319")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommonTestUtilities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommonTestUtilities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
